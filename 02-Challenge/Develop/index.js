661515583043c351471863f0e2543cc5a4102aaa
Blake <blakepetersont@gmail.com>
2022-11-18 14:15:19 -0700

    adding questions to the index

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,7 +1,36 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const license = {
+    'bsd3' : '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+    'bsd2' : '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)',
+    'apache' : '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+}
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installations for this project?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How should this project be used?"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
